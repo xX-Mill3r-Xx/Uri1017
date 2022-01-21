7beb1d0239f475e87b34aed962d621d107cea50f
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-21 08:26:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, velocidade;
+            double litros;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            int distancia = tempo * velocidade;
+            litros = distancia / 12.0;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
